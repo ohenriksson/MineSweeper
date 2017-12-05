d6e7c7faa6bbe59d01088fc17856e11698580668
Duane Irvin <irvin93d@gmail.com>
2017-12-05 19:11:54 +0100

    Init MineField Datastructure

---
+++ MineField.hs
@@ -0,0 +1,31 @@
+{- |
+Module      :  $Header$
+Description :  <optional short text displayed on contents page>
+Copyright   :  (c) <Authors or Affiliations>
+License     :  <license>
+
+Maintainer  :  irvin93d@gmail.com
+Stability   :  unstable | experimental | provisional | stable | frozen
+Portability :  portable | non-portable (<reason>)
+
+<module description starting at first column>
+-}
+
+{-
+TODO Implement Cell
+Should contain Mine or an integer 0-8
+May be open, closed or flaged
+-}
+
+{-
+TODO Implement Arbitrary Cell
+-}
+
+{-
+TODO Implement Board
+Should contain 2 dimensional grid of cells. Make it two lists
+-}
+
+{-
+TODO Implement Arbitrary Board
+-}
