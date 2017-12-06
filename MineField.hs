1b89a7c88cc221196ebeadea8565dbadf0d24e00
Duane Irvin <irvin93d@gmail.com>
2017-12-06 06:39:46 +0100

    Implement MineField.Cell

--- MineField.hs
+++ MineField.hs
@@ -11,11 +11,21 @@ Portability :  portable | non-portable (<reason>)
 <module description starting at first column>
 -}
 
+module MineField where
+
 {-
 TODO Implement Cell
 Should contain Mine or an integer 0-8
 May be open, closed or flaged
 -}
+data Cell = Cell { content :: CellContent, status :: CellStatus }
+            deriving (Eq, Show)
+
+data CellContent = Numeric Integer | Mine 
+                   deriving (Eq, Show)
+
+data CellStatus = Open | Closed | Flaged
+                  deriving (Eq, Show)
 
 {-
 TODO Implement Arbitrary Cell
