9cb8f8e8958a676de99395a2fd5d0cc6120bfecc
Oskar Henriksson <o.henriksson@gmail.com>
2017-12-07 12:19:55 +0100

    Add skeleton to RunGame

--- RunGame.hs
+++ RunGame.hs
@@ -1,1 +1,30 @@
-module RunGame
+{- |
+Module      :  $Header$
+Description :  <optional short text displayed on contents page>
+Copyright   :  (c) <Authors or Affiliations>
+License     :  <license>
+
+Maintainer  :  irvin93d@gmail.com
+Stability   :  unstable | experimental | provisional | stable | frozen
+Portability :  portable | non-portable (<reason>)
+
+<module description starting at first column>
+-}
+
+module RunGame where
+
+import MineSweeperEngine
+
+-- | start gameLoop on user input
+-- runGame :: Interface -> IO ()
+-- TODO
+
+
+-- | game loop, let user set up a new game and playMatch or exit.
+-- gameLoop :: Interface -> IO ()
+-- TODO
+
+
+-- | play one match, interact with user,
+-- playMatch :: Minefield -> IO()
+-- TODO
