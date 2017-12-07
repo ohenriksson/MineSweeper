fe82acdd9c82e32a37f7b9744eae74e83c6c093a
Duane Irvin <irvin93d@gmail.com>
2017-12-07 09:53:43 +0100

    Clean up ListFunctions

--- ListFunctions.hs
+++ ListFunctions.hs
@@ -1,8 +1,5 @@
 module ListFunctions where
 
-inRange :: (Eq a, Ord a) => a -> a -> a -> Bool
-inRange a b c = a <= b && b < c
-
 -- | For a given list l, and a given tuple (i, v),
 --   replace element i in l with v
 (!!=) :: [a] -> (Int,a) -> [a]
@@ -10,10 +7,18 @@ inRange a b c = a <= b && b < c
     | null l                     = error "(!!): List is empty."
     | not$inRange 0 i (length l) = error "(!!): Index out of range."
     | otherwise                  = l1 ++ [r] ++ l2
-    where (_, _, l1, l2) = pop i l
+    where (_, _, l1, l2) = pop l i
 
-pop :: Int -> [a] -> (a, [a], [a], [a])
-pop i l 
-    | not$inRange 0 i (length l - 1) = error "pop: Index out of range."
-    | otherwise = (head l2, l1 ++ (drop 1 l2), l1, drop 1 l2)
+-- | For three given elements, return True if second element
+--   is equal to or larger than first, but small than third
+inRange :: (Eq a, Ord a) => a -> a -> a -> Bool
+inRange a b c = a <= b && b < c
+
+-- | For a given list l, and a given index i, remove element i from l
+--   and return tuple with <element i>, <new list>, <elements before i>,
+--   <elements after i>
+pop :: [a] -> Int -> (a, [a], [a], [a])
+pop l i
+    | not$inRange 0 i (length l) = error "pop: Index out of range."
+    | otherwise = (head l2, l1 ++ drop 1 l2, l1, drop 1 l2)
     where (l1, l2) = splitAt i l
--- prop_ListFunctions.hs
+++ prop_ListFunctions.hs
@@ -1,9 +1,36 @@
 
 import ListFunctions
 import Test.QuickCheck
+
 -- | Test correct replacement in list
-prop_replaceInList_correct :: [Int] -> [Int] -> Int -> Int -> Property
-prop_replaceInList_correct l1 l2 old new = 
+prop_replaceInList :: [Int] -> [Int] -> Int -> Int -> Property
+prop_replaceInList l1 l2 old new = 
     not (null l2) && old /= new ==> 
         l1 ++ [new] ++ l2 == l !!= (length l1, new)
-    where l = (l1 ++ [new] ++ l2)
+    where l = l1 ++ [new] ++ l2
+
+-- | Test inRange, some basic cases
+prop_inRange :: Int -> Bool
+prop_inRange x =
+    not $inRange x x x
+    && not (inRange (x-1) x x)
+    && not (inRange (x+1) x x)
+    && not (inRange x x (x-1))
+    && inRange x x (x+1)
+    && not (inRange (x-1) x (x-1))
+    && inRange (x-1) x (x+1)
+    && not (inRange (x+1) x (x-1))
+    && not (inRange (x+1) x (x+1))
+
+-- | Test correct values from pop
+prop_pop :: [Int] -> [Int] -> Int -> Property
+prop_pop l1 l2 x = 
+    not (null l2) ==>
+        x' == x
+        && l1' == l1
+        && l2' == l2
+        && l' == l1 ++ l2
+    where
+        i = length l1
+        l = l1 ++ [x] ++ l2
+        (x', l', l1', l2') = pop l i
