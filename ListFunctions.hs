f309b7d84b845ed88d822baaf3c57af1dd1eeac7
Duane Irvin <irvin93d@gmail.com>
2017-12-08 11:33:59 +0100

    Implement count ListFunction

--- ListFunctions.hs
+++ ListFunctions.hs
@@ -7,7 +7,15 @@ module ListFunctions where
     | null l                     = error "(!!): List is empty."
     | not$inRange 0 i (length l) = error "(!!): Index out of range."
     | otherwise                  = l1 ++ [r] ++ l2
-    where (_, _, l1, l2) = pop l i
+    where (_, _, l1, l2) = pop i l
+
+-- | For two given lists, return the cartesian product
+cartesian :: [a] -> [b] -> [(a,b)]
+cartesian xs ys = [(x,y) | x <- xs, y <- ys]
+
+-- | Count occurances of given element in given list
+count :: (Eq a) => a -> [a] -> Int
+count x l = length $filter (x ==) l
 
 -- | For three given elements, return True if second element
 --   is equal to or larger than first, but small than third
@@ -17,8 +25,8 @@ inRange a b c = a <= b && b < c
 -- | For a given list l, and a given index i, remove element i from l
 --   and return tuple with <element i>, <new list>, <elements before i>,
 --   <elements after i>
-pop :: [a] -> Int -> (a, [a], [a], [a])
-pop l i
+pop :: Int -> [a] -> (a, [a], [a], [a])
+pop i l
     | not$inRange 0 i (length l) = error "pop: Index out of range."
     | otherwise = (head l2, l1 ++ drop 1 l2, l1, drop 1 l2)
     where (l1, l2) = splitAt i l
